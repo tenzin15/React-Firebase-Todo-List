0e16cbc79b63bbc250d2d7a39f7b8afdf4d6bfc3
Tenzin Chhosphel <tenchhos@gmail.com>
2017-01-12 15:26:52 -0500

    User can create todo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,103 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import moment from 'moment';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { todos: {} };
+
+    this.handleNewTodoInput = this.handleNewTodoInput.bind(this);
+  }
+
+  componentDidMount() {
+    axios({
+      url: '/todos.json',
+      baseURL: 'https://todo-793dc.firebaseio.com/',
+      method: 'GET'
+    })
+    .then((res) => {
+      this.setState({ todos: res.data })
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+
+  }
+  createTodo(todoText) {
+    // your code goes here
+    let newTodo = { title: todoText, createdAt: new Date };
+    axios({
+      url: '/todos.json',
+      baseURL: 'https://todo-793dc.firebaseio.com/',
+      method: 'POST',
+      data: newTodo
+    })
+    .then((res) => {
+      let todos = this.state.todos;
+      todos[res.data.time] = newTodo;
+      // set the state after pushing the todo item to firebase
+      this.setState({
+        todos
+      })
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  handleNewTodoInput(event) {
+    if (event.charCode === 13) {
+      this.createTodo(event.target.value);
+      event.target.value = "";
+    }
+  }
+
+
+  renderNewTodoBox() {
+    return (
+      <div className="new-todo-box pb-2">
+        <input className="w-100" placeholder="What do you have to do?" onKeyPress={ this.handleNewTodoInput } />
+      </div>
+    );
+  }
+
+  renderTodoList() {
+    let todoElements = [];
+
+    for(let todoId in this.state.todos) {
+      let todo = this.state.todos[todoId]
+
+      todoElements.push(
+        <div className="todo d-flex justify-content-between pb-4" key={todoId}>
+          <div className="mt-2">
+            <h4>{todo.title}</h4>
+            <div>{moment(todo.createdAt).calendar()}</div>
+          </div>
+        </div>
+      );
+    }
+
+    return (
+      <div className="todo-list">
+        {todoElements}
+      </div>
+    );
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="App container-fluid">
+        <div className="row pt-3">
+          <div className="col-6 px-4">
+            {this.renderNewTodoBox()}
+            {this.renderTodoList()}
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
 
 export default App;
+
---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAlBYlU4jyetWnUhiSVa-1F8iqNIYoXdWo",
+  authDomain: "todo-793dc.firebaseapp.com",
+  databaseURL: "https://todo-793dc.firebaseio.com",
+});
+
+export default base;
