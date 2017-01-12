95767cb8cbf26baae4222d111974b51815ce6181
Tenzin Chhosphel <tenchhos@gmail.com>
2017-01-12 15:53:36 -0500

    User can edit a todo

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
     this.handleNewTodoInput = this.handleNewTodoInput.bind(this);
     this.deleteTodo = this.deleteTodo.bind(this);
     this.selectTodo = this.selectTodo.bind(this);
+    this.enableEditMode = this.enableEditMode.bind(this);
+    this.updateCurrentTodo = this.updateCurrentTodo.bind(this);
   }
 
   componentDidMount() {
@@ -66,16 +68,53 @@ class App extends Component {
     this.setState({ currentTodo: todoId });
   }
 
+  enableEditMode() {
+    this.setState({ edit: true });
+  }
+
+  updateCurrentTodo() {
+    let id = this.state.currentTodo;
+    let currentTodo = this.state.todos[id];
+    currentTodo.title = this.refs.editTodoInput.value;
+
+    axios({
+      url: `/todos/${id}.json`,
+      baseURL: 'https://todo-793dc.firebaseio.com/',
+      method: "PATCH",
+      data: currentTodo
+    }).then((response) => {
+      let todos = this.state.todos;
+      todos[id] = currentTodo;
+      this.setState({ todos: todos, edit: false });
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
   renderSelectedTodo() {
     let content;
 
     if (this.state.currentTodo) {
       let currentTodo = this.state.todos[this.state.currentTodo];
-      content =  (
-        <div>
-          <h1>{currentTodo.title}</h1>
-        </div>
-      );
+      if(!this.state.edit) {
+        content =  (
+          <div>
+            <div className="d-flex justify-content-end mb-3">
+              <button onClick={this.enableEditMode}>Edit</button>
+            </div>
+            <h1>{currentTodo.title}</h1>
+          </div>
+        );
+      } else {
+        content =  (
+          <div>
+            <div className="d-flex justify-content-end mb-3">
+              <button onClick={this.updateCurrentTodo}>Save</button>
+            </div>
+            <input className="w-100" defaultValue={currentTodo.title} ref="editTodoInput" />
+          </div>
+        );
+      }
     }
 
     return content;
@@ -96,7 +135,7 @@ class App extends Component {
     );
   }
 
-   renderTodoList() {
+    renderTodoList() {
     let todoElements = [];
 
     for(let todoId in this.state.todos) {
